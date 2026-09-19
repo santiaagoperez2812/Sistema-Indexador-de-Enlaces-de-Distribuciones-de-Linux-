196678186cd71edfb704e9990973517cfa36f3f3
Santi <srpv151@gmail.com>
2026-09-19 18:19:50 -0400

    ci: configurar despliegue automatico de Astro a GitHub Pages

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://santiaagoperez2812.github.io',
+  base: '/Sistema-Indexador-de-Enlaces-de-Distribuciones-de-Linux-',
+});
