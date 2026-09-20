92f7395eda6b6a623b97ce45b221b72467aa9ee6
Santi <srpv151@gmail.com>
2026-09-19 23:20:45 -0400

    fix: corregir enlaces caidos y logos sin imagenes

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -3,4 +3,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://santiaagoperez2812.github.io',
   base: '/Sistema-Indexador-de-Enlaces-de-Distribuciones-de-Linux-',
+  trailingSlash: 'always'
 });
